40524f394142bed22b4cc56ebe88e5eaa6366dff
Subhajit Chatterjee <subhajt.bdk@gmail.com>
2023-06-09 15:44:54 +0530

    Initialized project environment

---
+++ meme_monster/meme_monster/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for meme_monster project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meme_monster.settings')
+
+application = get_asgi_application()
