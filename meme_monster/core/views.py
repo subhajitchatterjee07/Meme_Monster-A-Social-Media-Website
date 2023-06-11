c1ce3ae24f1293cdb653f529f9de8575ed921d5e
Subhajit Chatterjee <subhajt.bdk@gmail.com>
2023-06-11 10:41:42 +0530

    completed sign up, authentication, and serving static files

--- meme_monster/core/views.py
+++ meme_monster/core/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Profile
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        print(username)
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'Username taken')
+                return redirect('signup')
+            else:
+                user= User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+
+                #log user in and redirect to the settings page
+                #create a profile object 
+
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user = user_model,id_user= user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request,'Password not Matching')
+            return redirect('signup')
+    else:
+        return render(request,'signup.html')
