53084a3b6b5b1b551b83f16ab9e711c219f00656
Subhajit Chatterjee <subhajt.bdk@gmail.com>
2023-06-11 23:11:01 +0530

    Signin and Logout

--- meme_monster/core/urls.py
+++ meme_monster/core/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('signup',views.signup,name='signup')
+    path('signup',views.signup,name='signup'),
+    path('signin',views.signin,name='signin'),
+    path('logout',views.logout,name='logout'),
     
-
 ]
--- meme_monster/core/views.py
+++ meme_monster/core/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='signin')
 def index(request):
     return render(request,'index.html')
 
@@ -38,3 +41,25 @@ def signup(request):
             return redirect('signup')
     else:
         return render(request,'signup.html')
+
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Credentials Invalid')
+            return redirect('signin')
+    return render (request,'signin.html')
+
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
