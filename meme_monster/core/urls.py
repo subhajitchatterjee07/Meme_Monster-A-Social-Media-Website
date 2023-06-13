00311925baa464040eb44356b2269140851519fd
Subhajit Chatterjee <subhajt.bdk@gmail.com>
2023-06-14 00:02:57 +0530

    Profile Management Part 1

--- meme_monster/core/urls.py
+++ meme_monster/core/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout',views.logout,name='logout'),
     path('upload',views.upload,name='upload'),
     path('like-post',views.like_post,name='like-post'),
+    path('profile/<str:pk>',views.profile,name='profile'),
 ]
--- meme_monster/core/views.py
+++ meme_monster/core/views.py
@@ -72,6 +72,19 @@ def signup(request):
         return render(request,'signup.html')
 
 
+@login_required(login_url='signin')
+def profile(request,pk):
+    user_object = User.objects.get(username=pk)
+    user_profile = Profile.objects.get(user=user_object)
+    user_posts = Post.objects.filter(user=pk)
+    user_post_length = len(user_posts)
+    context = {
+        'user_object': user_object,
+        'user_profile': user_profile,
+        'user_posts': user_posts,
+        'user_post_length': user_post_length,
+    }
+    return render(request,'profile.html',context)
 
 def signin(request):
     if request.method == 'POST':
