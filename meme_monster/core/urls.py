c6eaf4c5ac72dcda8b309feea57c4a64a6778643
Subhajit Chatterjee <subhajt.bdk@gmail.com>
2023-06-14 18:12:25 +0530

    Follow and Unfollow users

--- meme_monster/core/urls.py
+++ meme_monster/core/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('upload',views.upload,name='upload'),
     path('like-post',views.like_post,name='like-post'),
     path('profile/<str:pk>',views.profile,name='profile'),
+    path('follow',views.follow,name='follow'),
 ]
--- meme_monster/core/views.py
+++ meme_monster/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile,Post,LikePost
+from .models import Profile,Post,LikePost,FollowersCount
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -78,13 +78,31 @@ def profile(request,pk):
     user_profile = Profile.objects.get(user=user_object)
     user_posts = Post.objects.filter(user=pk)
     user_post_length = len(user_posts)
+
+    follower = request.user.username
+    user = pk
+
+    if FollowersCount.objects.filter(follower=follower,user=user).first():
+        button_text = 'Unfollow'
+    else:
+        button_text ='Follow'
+        
+    user_followers = len(FollowersCount.objects.filter(user=pk))
+    user_following = len(FollowersCount.objects.filter(follower=pk))
+
+
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_posts': user_posts,
         'user_post_length': user_post_length,
+        'button_text' : button_text,
+        'user_followers': user_followers,
+        'user_following': user_following,
     }
     return render(request,'profile.html',context)
+
+
 
 def signin(request):
     if request.method == 'POST':
@@ -99,6 +117,8 @@ def signin(request):
             messages.info(request,'Credentials Invalid')
             return redirect('signin')
     return render (request,'signin.html')
+
+
 
 
 @login_required(login_url='signin')
@@ -138,6 +158,7 @@ def settings(request):
     return render(request, 'setting.html',{'user_profile': user_profile})
 
 
+@login_required(login_url='signin')
 def upload(request):
     if request.method =='POST':
         user = request.user.username
@@ -148,4 +169,22 @@ def upload(request):
         return redirect('/')
     else:
         return redirect('/')
+
+
+@login_required(login_url='signin')
+def follow(request):
+    if request.method =='POST':
+        follower = request.POST['follower']
+        user = request.POST['user']
+
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower,user=user)
+            delete_follower.delete()
+            return redirect ('/profile/'+user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower,user=user)
+            new_follower.save()
+            return redirect ('/profile/'+user)
+    else:
+        return redirect ('/')
 
