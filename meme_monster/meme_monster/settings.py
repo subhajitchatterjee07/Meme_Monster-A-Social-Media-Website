a011d9869c49821b45c4e2b7df1bc1a84a5506e9
Subhajit Chatterjee <subhajt.bdk@gmail.com>
2023-06-23 13:38:59 +0530

    working on jsonfiles

--- meme_monster/meme_monster/settings.py
+++ meme_monster/meme_monster/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-cuy&86=bx@#%&r9q=hw_cx)v&h_8lc*=&@xr!$mcv=p4qrp%%i
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app' '.now.sh']
 
 
 # Application definition
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
